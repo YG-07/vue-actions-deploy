e0757a4de52ee52f82a312b87fa2a6b1c9058e24
dusw <siwei.du@hantotem.com>
2021-07-20 17:57:05 +0800

    OK~

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import run from '@jamesives/github-pages-deploy-action'
 run({
   token: process.env['ACCESS_TOKEN'],
   branch: 'gh-pages',
-  folder: 'build',
+  folder: 'dist',
   repositoryName: 'JamesIves/github-pages-deploy-action',
   silent: true,
   workspace: 'src/project/location'
