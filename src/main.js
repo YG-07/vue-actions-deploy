fac771d51adad09b7c2e7d4e4e1a0186f251e42f
dusw <siwei.du@hantotem.com>
2021-07-20 18:05:45 +0800

    update~

--- src/main.js
+++ src/main.js
@@ -1,15 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import run from '@jamesives/github-pages-deploy-action'
-
-run({
-  token: process.env['ACCESS_TOKEN'],
-  branch: 'gh-pages',
-  folder: 'dist',
-  repositoryName: 'JamesIves/github-pages-deploy-action',
-  silent: true,
-  workspace: 'src/project/location'
-})
 
 Vue.config.productionTip = false
 
