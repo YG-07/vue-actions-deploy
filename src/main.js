a8ea2c5acc59ae37ed8861c9493e91b2370f8896
dusw <siwei.du@hantotem.com>
2021-07-20 17:17:34 +0800

    Ok!

--- src/main.js
+++ src/main.js
@@ -1,15 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import run from '@jamesives/github-pages-deploy-action'
 
-run({
-  token: process.env['ACCESS_TOKEN'],
-  branch: 'gh-pages',
-  folder: 'build',
-  repositoryName: 'JamesIves/github-pages-deploy-action',
-  silent: true,
-  workspace: 'src/project/location'
-})
 
 Vue.config.productionTip = false
 
