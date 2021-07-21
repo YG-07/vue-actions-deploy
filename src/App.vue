e38747215e7c65456bc339ff5d4cc5d7b72e234f
dusw <siwei.du@hantotem.com>
2021-07-21 09:25:52 +0800

    update ok

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <h2>Github.io 个人页面</h2>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -12,6 +13,9 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  created() {
+    console.log('Hello Github.io');
   }
 }
 </script>
